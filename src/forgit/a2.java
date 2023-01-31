47a6bc610437da613600d9a4ced5a1ac9b03fc87
unknown <sagar1000021@gmail.com>
2023-01-31 13:16:38 +0530

    add another class

---
+++ src/forgit/a2.java
@@ -0,0 +1,21 @@
+package forgit;
+
+public class a2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		
+		
+		int a=7;
+		int b=10;
+		
+		int sub= b-a;
+		
+		System.out.println("sub is "+ sub);
+
+
+	}
+
+}
